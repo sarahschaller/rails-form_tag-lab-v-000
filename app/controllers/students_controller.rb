6e75b8e720307ffd8b13b2528f88dee10a5b9c7c
Sarah Schaller <sarcsch@aol.com>
2017-10-29 23:37:20 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,9 +9,9 @@ class StudentsController < ApplicationController
   end
 
   def new
-  end 
+  end
 
-  def create 
+  def create
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to students_path
   end
